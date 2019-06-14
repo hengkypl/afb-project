7c8a59141fabd1329c260353d012014dfd1d7b68
Bayu <bayu.setiahartono@lazada.co.id>
2019-06-14 17:11:51 +0700

    Fix hour meter form report

--- report/views.py
+++ report/views.py
@@ -6,7 +6,6 @@ from django.urls import reverse
 from alatberat.models import Hourmeter
 
 from .forms import HourmeterReportForm
-from .utils import indo_date_to_iso
 
 
 # Create your views here.
@@ -29,8 +28,8 @@ class HourmeterReportView(ListView):
             if all(key in get_var for key in self.valid_keys):
                 alat_id = get_var['alatid']
                 operator_id = get_var['operatorid']
-                start_date = indo_date_to_iso(get_var['start_date'])
-                end_date = indo_date_to_iso(get_var['end_date'])
+                start_date = get_var['start_date']
+                end_date = get_var['end_date']
 
                 queryset = self.model.objects.filter(
                     alatid_id=int(alat_id), operatorid_id=int(operator_id), tanggal__range=[start_date, end_date]
@@ -57,8 +56,8 @@ class HourmeterReportView(ListView):
             if all(key in post_var for key in self.valid_keys):
                 form = HourmeterReportForm(data=post_var)
                 if form.is_valid():
-                    alatid = form.cleaned_data['alatid']
-                    operatorid = form.cleaned_data['operatorid']
+                    alatid = form.cleaned_data['alatid'].id
+                    operatorid = form.cleaned_data['operatorid'].id
                     start_date = form.cleaned_data['start_date']
                     end_date = form.cleaned_data['end_date']
 
