185732ca98e744cb13d0a7b176d62f4db8dbbb4f
Bayu <bayu.setiahartono@lazada.co.id>
2019-06-16 23:37:26 +0700

    Upload missing file

---
+++ afb/views.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+
+from afb.auth import UserModelBackend
+
+User = get_user_model()
+
+
+class LoginView(TemplateView):
+    template_name = 'signin.html'
+
+    def render_to_response(self, context, *args, **kwargs):
+        response = super().render_to_response(context, *args, **kwargs)
+        if self.request.user.is_authenticated:
+            response = HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+        return response
+
+    def post(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            userbackend = UserModelBackend()
+            user = userbackend.authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(self.request, user)
+                user_logged_in.send(sender=User, request=request, user=user)
+                messages.add_message(self.request, messages.SUCCESS, 'Logged In')
+            else:
+                messages.add_message(self.request, messages.ERROR, "User is inactive")
+        else:
+            messages.add_message(self.request, messages.ERROR, "Your email or password is incorrect.")
+        return HttpResponseRedirect(settings.LOGIN_URL)
+
+
+class LogoutView(TemplateView, LoginRequiredMixin):
+    def render_to_response(self, context, *args, **kwargs):
+        User = get_user_model()
+        user = self.request.user
+        logout(self.request)
+        user_logged_out.send(sender=User, request=self.request, user=user)
+        messages.add_message(self.request, messages.SUCCESS, "Logged Out")
+        return HttpResponseRedirect(settings.LOGIN_URL)
